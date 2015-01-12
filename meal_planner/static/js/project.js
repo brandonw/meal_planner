d23c8332669880c51311ca084de02c7caed01a24
Brandon Waskiewicz <brandon.waskiewicz@gmail.com>
2015-01-11 20:31:35 -0500

    Add ajax form post for deleting dayrecipes

--- meal_planner/static/js/project.js
+++ meal_planner/static/js/project.js
@@ -1,5 +1,14 @@
 /* Project specific Javascript goes here. */
 
 $(document).ready(function() {
-    $('a.tooltip-a').tooltip()
+    $('a.tooltip-a').tooltip();
 });
+
+var removeDayRecipe = function(e) {
+    e.preventDefault();
+    var form = $(this);
+    var postUrl = form.attr('action');
+    $.post(postUrl, form.serialize()).done(function (data) {
+        form.closest('li.list-group-item').remove();
+    });
+};
