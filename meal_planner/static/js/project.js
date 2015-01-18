3b41f0297635c4b3cacf39d521253749ef0cec13
Brandon Waskiewicz <brandon.waskiewicz@gmail.com>
2015-01-17 22:32:49 -0500

    Enable ajax get request to prevent page reload
    When rating a recipe, use ajax if javascript is enabled to prevent
    the need for a page reload.

--- meal_planner/static/js/project.js
+++ meal_planner/static/js/project.js
@@ -12,3 +12,20 @@ var removeDayRecipe = function(e) {
         form.closest('li.list-group-item').remove();
     });
 };
+
+var updateRating = function(e) {
+    e.preventDefault();
+    var anchor = $(e.target);
+    $.get(anchor.attr('href'), function (data) {
+        var rating = anchor.text().trim();
+        var recipeID = anchor.closest('li.list-group-item')
+            .find('input:hidden').val();
+
+        // when updating rating, make sure to update any other instances
+        // of this recipe already displayed on the page
+        var ratings = $('input:hidden[value=' + recipeID + ']').parent()
+            .find('ul#dayrecipe-rating');
+        ratings.removeClass();
+        ratings.addClass('rating star' + rating);
+    });
+};
